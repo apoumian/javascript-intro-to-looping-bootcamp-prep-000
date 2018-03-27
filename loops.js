df751a6ef77287cc44477b7e6464d4b3fa460936
Arturo Poumian <apoumians@gmail.com>
2018-03-27 12:53:55 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -20,6 +20,6 @@ function whileLoop(countdown) {
 
 function pregunton(numMagic) {
   do {
-    numMagic = Number(prompt("Cual es el número mágico??));
-  } while (numMagic === 48);
+    numMagic = Number(prompt("Cual es el número mágico??"));
+  } while (numMagic !== 48);
 }
